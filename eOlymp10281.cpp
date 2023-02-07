c7dcb8584b2136404e7abdfb1fb051046875df2e
Svartorm <hugo.frangiamone@gmail.com>
2023-02-07 11:17:12 +0700

    Init

---
+++ eOlymp10281.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+int n, height, ans;
+
+int main() {
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &height);
+        ans = max(ans, height);
+    }
+    printf("%d\n", ans);
+    return 0;
+}
